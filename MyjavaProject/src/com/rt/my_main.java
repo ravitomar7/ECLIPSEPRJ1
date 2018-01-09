8878c4cb514b93f64bdcdac8fe1ab565275b619f
hkshitesh <hkshitesh@gmail.com>
2018-01-09 13:35:55 +0530

    dfdasd

--- MyjavaProject/src/com/rt/my_main.java
+++ MyjavaProject/src/com/rt/my_main.java
@@ -4,7 +4,7 @@ public class my_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HELLO WORLD");
+		System.out.println("Bhai");
 		System.out.println("Changed");
 
 	}
